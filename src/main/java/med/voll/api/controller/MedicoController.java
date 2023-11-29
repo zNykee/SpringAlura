8277bc86c31b2c8163c28e4d7ba47624e40d7afa
zNykee <nicholas.lima.torres@gmail.com>
2023-11-29 15:29:21 -0300

    AluraSpring

---
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+
+import jakarta.validation.Valid;
+import med.voll.api.medico.DadosCadrastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/medicos")
+
+public class MedicoController {
+
+    @Autowired
+    private MedicoRepository repository ;
+
+    @PostMapping
+    @Transactional
+    public void cadrastrar(@RequestBody @Valid DadosCadrastroMedico dados){
+        repository.save(new Medico(dados));
+
+    }
+}
---
+++ src/main/java/med/voll/api/endereco/DadosEndereco.java
@@ -0,0 +1,27 @@
+package med.voll.api.endereco;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
+public record DadosEndereco(
+
+        @NotBlank
+        String logradouro,
+
+        @NotBlank
+        String bairro,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{8}")
+        String cep ,
+
+        String numero ,
+
+        String complemento,
+
+        @NotBlank
+        String cidade ,
+
+        @NotBlank
+        String uf) {
+}
